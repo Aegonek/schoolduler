6223825183d18cec373a86e46d7edd70b58d4e65
Aegonek <dominikjachymczyk@gmail.com>
2022-07-14 11:54:07 +0200

    Fixed rating function.

--- src/algen/solutions/theta/fitness_ops.rs
+++ src/algen/solutions/theta/fitness_ops.rs
@@ -7,9 +7,14 @@ pub fn inverse_of_no_class_conflicts(solver: &Solution, chromosome: &Chromosome)
     // Worst result - every class has every lesson scheduled for the same hour...
     // and every teacher has every lesson scheduled for the same hour.
 
-    let decoded = solver.decode(chromosome);
+    let mut max_possible_conflicts: usize = 0;
+    let lessons = solver.decode(chromosome);
+    let teacher_conflicts = overlapping_lessons_for_teacher(&lessons, &mut max_possible_conflicts);
+    let class_conflicts = overlapping_lessons_for_class(&lessons, &mut max_possible_conflicts);
+    let ratio = (teacher_conflicts + class_conflicts) as f64 / max_possible_conflicts as f64;
+    let rating = 1.0 / ratio;
 
-    todo!();
+    (rating * 100_000.0)  as u32
 }
 
 // TODO: test
@@ -23,6 +28,25 @@ fn overlapping_lessons_for_teacher(lessons: &[Class], max_possible_conflicts: &m
         *max_possible_conflicts += teacher_group.len();
         teacher_group.sort_by_key(|x| x.lesson_hour);
         let hour_groups = teacher_group.into_iter().group_by(|x| x.lesson_hour.clone());
+        for hour_group in hour_groups.into_iter() {
+            let len = hour_group.1.count();
+            if len > 1 { conflicts += len }
+        }
+    }
+
+    conflicts
+}
+
+fn overlapping_lessons_for_class(lessons: &[Class], max_possible_conflicts: &mut usize) -> usize {
+    let mut conflicts: usize = 0;
+    let mut working_copy = lessons.to_owned();
+    working_copy.sort_by_key(|x| x.teacher.clone());
+    let student_groups = working_copy.into_iter().group_by(|x| x.student_group.clone());
+    for student_group in student_groups.into_iter() {
+        let mut student_group: Vec<_> = student_group.1.collect();
+        *max_possible_conflicts += student_group.len();
+        student_group.sort_by_key(|x| x.lesson_hour);
+        let hour_groups = student_group.into_iter().group_by(|x| x.lesson_hour.clone());
         for hour_group in hour_groups.into_iter() {
             let len = hour_group.1.count();
             if len > 1 { conflicts += len }
